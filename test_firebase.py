610b41b2479c52e13f789b3dd5de31defb17fbfb
Periyaraja <pmperiyaraja@gmail.com>
2025-08-29 15:00:39 +0530

    Update test_firebase.py

--- test_firebase.py
+++ test_firebase.py
@@ -1,11 +1,1 @@
-from firebase_utils import log_event
 
-event = {
-    "animal": "test_animal",
-    "confidence": 0.99,
-    "time": "2025-08-23T12:00:00",
-    "snapshot": None
-}
-
-print("🚀 Sending test event to Firestore...")
-log_event(event)
